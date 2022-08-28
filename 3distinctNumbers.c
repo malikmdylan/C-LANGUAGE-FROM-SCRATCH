01b0299b4fd53e429086859025de1168efaf5077
Malik Mubarak Dylan <malikcquantum@gmail.com>
2022-08-28 12:18:24 +0530

    Add files via upload

---
+++ 3distinctNumbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+   int a, b,c;
+ printf("Enter number a,b,c : ");
+    scanf("%d%d%d", &a,&b,&c);
+
+   if(a > b && a > c)
+      printf("a is largest ");
+   else  if(b > a && b > c)
+      printf("b is largest ");
+    else  if(c > a && c > b)
+      printf("c is largest ");
+    else printf("No largest number found")  ;
+
+   return 0;
+}
