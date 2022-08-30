0d29d277eccc21002d456e4b85bb0905af2ff74a
Malik Mubarak Dylan <malikcquantum@gmail.com>
2022-08-30 17:27:56 +0530

    Add files via upload

---
+++ cubeOfFirstN.c
@@ -0,0 +1,12 @@
+//find cube of first n natural numbers
+#include <stdio.h>
+int main() {
+    int n, i, sum = 0;
+    printf("Enter number n : ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; ++i) {
+        sum = sum + i*i*i;
+    }
+    printf("Cube of first %d natural numbers is %d", n, sum);
+    return 0;
+}
