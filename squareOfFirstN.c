92bba357b464922c917b2ffe0b9d74b4137a2f70
Malik Mubarak Dylan <malikcquantum@gmail.com>
2022-08-30 17:19:39 +0530

    Add files via upload

---
+++ squareOfFirstN.c
@@ -0,0 +1,12 @@
+//find square of first n natural numbers
+#include <stdio.h>
+int main() {
+    int n, i, sum = 0;
+    printf("Enter number n : ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; ++i) {
+        sum = sum + i*i;
+    }
+    printf("Square of first %d natural numbers is %d", n, sum);
+    return 0;
+}
