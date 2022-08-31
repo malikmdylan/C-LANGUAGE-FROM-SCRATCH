f19f0f086a6ca1796d3fb9a90c6de07061e88dbd
Malik Mubarak Dylan <malikcquantum@gmail.com>
2022-08-31 21:13:00 +0530

    Add files via upload

---
+++ tempChange.c
@@ -0,0 +1,11 @@
+//change temperature from celsius to fahrenheit
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    float c,f;
+    printf("enter the temperature in celsius");
+    scanf("%f",&c);
+    f=(c*9/5)+32;
+    printf("the temperature in fahrenheit is %f",f);
+}
