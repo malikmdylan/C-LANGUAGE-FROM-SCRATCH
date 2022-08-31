0f1114f78a83870bbce2078b4d356166a1e94667
Malik Mubarak Dylan <malikcquantum@gmail.com>
2022-08-31 21:17:47 +0530

    Add files via upload

---
+++ evenSeries.c
@@ -0,0 +1,15 @@
+//print even series.
+#include<stdio.h>
+void main()
+{
+    int i,n;
+    printf("enter the number of terms");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d\n",i);
+        }
+    }
+}
