cbdbf896a124c9510c148310d0e7cf2c16cece7b
Malik Mubarak Dylan <malikcquantum@gmail.com>
2022-09-01 20:18:59 +0530

    Add files via upload

---
+++ macros.c
@@ -0,0 +1,10 @@
+//use macros to find greatest of three numbers
+#include<stdio.h>
+#define greater(a,b) if (a>b) printf("%d is greater",a); else printf("%d is greater",b);
+void main()
+{
+    int a,b;
+    printf("enter two numbers");
+    scanf("%d%d",&a,&b);
+    greater(a,b);
+}
