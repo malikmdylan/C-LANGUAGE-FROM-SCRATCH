63300408bb7a94bed1eb4849c280a92b946eca6a
Malik Mubarak Dylan <malikcquantum@gmail.com>
2022-09-04 17:58:06 +0530

    Add files via upload

---
+++ inverseStarTriangle.c
@@ -0,0 +1,17 @@
+//print a downward triangular pattern of stars,take input from user.
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("Enter number of rows: ");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
