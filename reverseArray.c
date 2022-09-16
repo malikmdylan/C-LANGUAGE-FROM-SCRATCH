a4160efe9254d9283b342c4abbcf26b91c4af6fa
Malik Mubarak Dylan <malikcquantum@gmail.com>
2022-09-16 22:21:36 +0530

    Add files via upload

---
+++ reverseArray.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int i, n, x, flag = 0;
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter the elements of array: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for(i=0;i<=n-1;i++) 
+    {
+        printf("%d",a[i]);
+    }
+    printf("\n");
+    for(i=n-1;i>=0;i--)
+    {
+        printf("%d",a[i]);
+    }
+    return 0;
+}
