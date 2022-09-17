ccceca9a915aa18733b675b16b70ea28e79791e7
Malik Mubarak Dylan <malikcquantum@gmail.com>
2022-09-17 21:47:17 +0530

    Update monthUsingSwitch.c

--- monthUsingSwitch.c
+++ monthUsingSwitch.c
@@ -4,31 +4,31 @@
 int main()
 {
     int month;
-    printf("Enter the month number: ");
+    printf("Enter the month number: ")
     scanf("%d",&month);
     switch(month)
     {
-        case 1: printf("January  \n ");
+        case 1: printf("January  \n ")
             break;
-        case 2: printf("February  \n ");
+        case 2: printf("February  \n ")
             break;
-        case 3: printf("March  \n ");
+        case 3: printf("March  \n ")
             break;
-        case 4: printf("April   ");
+        case 4: printf("April   ")
             break;
-        case 5: printf("May \n  ");
+        case 5: printf("May \n  ")
             break;
-        case 6: printf("June \n ");
+        case 6: printf("June \n ")
             break;
-        case 7: printf("July  \n ");
+        case 7: printf("July  \n ")
             break;
-        case 8: printf("August  \n ");
+        case 8: printf("August  \n ")
             break;
-        case 9: printf("September  \n ");
+        case 9: printf("September  \n ")
             break;
-        case 10: printf("October  \n ");
+        case 10: printf("October  \n ")
             break;
-        case 11: printf("November  \n ");
+        case 11: printf("November  \n ")
             break;
         case 12: printf("December  \n ");
             break;
