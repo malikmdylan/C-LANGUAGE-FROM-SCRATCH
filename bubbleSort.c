a750949cac271eba524c0c398be7d71786d7c4b3
Malik Mubarak Dylan <malikcquantum@gmail.com>
2022-09-21 21:49:10 +0530

    Add files via upload

---
+++ bubbleSort.c
@@ -0,0 +1,29 @@
+//bubble sort in c
+#include <stdio.h>
+void bubbleSort(int arr[], int n)
+{
+    int temp;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+int main()
+{
+    int arr[] = {7, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
